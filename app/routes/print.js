76c427e8579bca1d458c1f01f5d1b015226a065e
Kevin Vongmany <kevin.vongmany@gmail.com>
2025-08-05 17:21:15 +1000

    created graphql query to fetch order data

--- app/routes/print.js
+++ app/routes/print.js
@@ -1,4 +1,4 @@
-import {authenticate} from '../shopify.server';
+import { authenticate } from '../shopify.server';
 
 export async function loader({ request }) {
     const { cors, admin } = await authenticate.admin(request);
@@ -6,6 +6,26 @@ export async function loader({ request }) {
     const query = url.searchParams;
     const docs = query.get("printType").split(",");
     const orderId = query.get("orderId");
+    const response = await admin.graphql(
+        `query getOrder(?orderId: ID!) {
+        order(id: $orderId) {
+            name
+            createdAt
+            totalPriceSet {
+                shopMoney {
+                    amount
+                }
+            }
+        }
+    }`,
+        {
+            variables: {
+                orderId
+            },
+        }
+    );
+    const orderData = await response.json();
+    const order = orderData.data.order;
 
     return null; // Placeholder for actual response handling
 }

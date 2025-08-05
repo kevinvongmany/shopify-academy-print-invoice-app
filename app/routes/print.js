793acefa5bb981a8a02fd5221c0893b1d7a7d17c
Kevin Vongmany <kevin.vongmany@gmail.com>
2025-08-05 17:16:35 +1000

    added authentication and destructured cors and admin funcs

--- app/routes/print.js
+++ app/routes/print.js
@@ -1,6 +1,8 @@
 import {authenticate} from '../shopify.server';
 
 export async function loader({ request }) {
+    const { cors, admin } = await authenticate.admin(request);
 
-  return null;
+
+    return null; // Placeholder for actual response handling
 }
